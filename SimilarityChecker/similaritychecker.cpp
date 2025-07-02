765b4ce122c1754a4a47d2c0485786f3f6d9b741
unknown <jwon96.park@gmail.com>
2025-07-02 15:38:03 +0900

    [feature] calculate length score

--- SimilarityChecker/similaritychecker.cpp
+++ SimilarityChecker/similaritychecker.cpp
@@ -7,4 +7,22 @@ public:
 		if (input1 == input2) return 100;
 		return score;
 	}
+
+	int getLengthScore(const std::string& input1, const std::string& input2) {
+		int length1 = input1.length();
+		int length2 = input2.length();
+		if (length1 == length2) return 60;
+		
+		int gap = 0;
+		if (length1 > length2) {
+			if (length1 >= length2 * 2) return 0;
+			gap = length1 - length2;
+			return 60 - ((60 * gap) / length2);
+		}
+		else {
+			if (length1 * 2 <= length2) return 0;
+			gap = length2 - length1;
+			return 60 - ((60 * gap) / length1);
+		}
+	}
 };
--- SimilarityChecker/similaritychecker_test.cpp
+++ SimilarityChecker/similaritychecker_test.cpp
@@ -11,6 +11,13 @@ TEST(TS, TC1) {
 	EXPECT_EQ(expected, score);
 }
 
+TEST(TS, TC2) {
+	SimilarityChecker checker;
+	int expected = 40;
+	int score = checker.getLengthScore("abcd", "abc");
+	EXPECT_EQ(expected, score);
+}
+
 int main() {
 	InitGoogleMock();
 	return RUN_ALL_TESTS();
